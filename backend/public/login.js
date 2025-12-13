ee6b8c64039f0716e64a65f8f28ecfb114358a94
Joseph Adeyemi <adeyemiolufemi421@gmail.com>
2025-12-13 15:44:41 +0000

    Test

--- backend/public/login.js
+++ backend/public/login.js
@@ -22,7 +22,7 @@ document.querySelector("#loginForm").addEventListener("submit", async (e) => {
     }
 
     // Save login session
-    localStorage.setItem("loggedInUser", email);
+    localStorage.setItem("loggedInUser", data.token);
 
     window.location.href = "index.html";
 });
